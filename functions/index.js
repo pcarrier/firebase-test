d0e1f0cdb3c4c42f46d0f283a1832e10aab558fd
Pierre Carrier <pierre@meteor.com>
2017-03-10 00:42:01 -0500

    lightweight version

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,6 @@
-require('@google-cloud/debug-agent').start();
-
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const murmur3 = require('node-murmur3');
 
 admin.initializeApp(functions.config().firebase);
 
-exports.hello = functions.https.onRequest((req, resp) => resp.send(murmur3.hash32(req.body).toString(16)));
+exports.hello = functions.https.onRequest((req, resp) => resp.send('hello!'));
